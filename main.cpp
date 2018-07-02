8ebca8ffe43a7b46d7285ad782a8ded127a4f7d6
Zhaeong <owen_zzz@hotmail.com>
2018-07-02 17:54:03 -0400

    adding a fps cap

--- main.cpp
+++ main.cpp
@@ -10,6 +10,15 @@ int main(int argc, char *argv[])
 {
     SDLHandler SH(640, 480);
 
+    const int FPS = 60;
+
+    //How many miliseconds per frame
+    const int frameDelay = 1000/FPS;
+
+    Uint32 frameStart;
+    float frameTime;
+
+
     GameObj playerObj(0, 0, SH.renderer);
     playerObj.loadImage("res/bikeMan.bmp");
 
@@ -17,17 +26,26 @@ int main(int argc, char *argv[])
 
     while (1)
     {
+        frameStart = SDL_GetTicks();
+
         string eventName = SH.getEvent();
 
         if(eventName == "MOVE_LEFT")
         {
             cout << eventName;
             GameObj player = SH.gameObjectArray.at(0);
-            player.x_location += 1;
+            player.x_location += 5;
 
             SH.gameObjectArray[0] = player;
         }
         SH.renderGameObjects();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if(frameDelay > frameTime)
+        {
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
 
 
